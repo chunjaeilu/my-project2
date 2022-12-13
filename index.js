e1b2fe778fa5a5bd6c88a610844c881c0514dbf6
chunjaeilu <chunjaeilu@naver.com>
2022-12-13 12:57:45 +0900

    1

--- index.js
+++ index.js
@@ -19,17 +19,17 @@ const pokemons = [
   },
 ];
 
-// pokemon 요청
+// parameter값(매개변수)을 활용한 페이지 요청
 app.get("/pokemon/:id", (req, res) => {
   console.log(req.params);
-  res.send(
-    `<img src="${pokemons[req.params.id - 1].url}" alt="${
-      req.params.id
-    }" /> <h2>${pokemons[req.params.id - 1].name}</h2>`
-  );
+  const html = `
+  <img src="${pokemons[req.params.id].url}" alt="${req.params.id}" />
+  <h2>${pokemons[req.params.id].name}</h2>
+  `;
+  res.send(html);
 });
 
-// get 요청
+// get 요청 (라우팅)
 app.get("/", (req, res) => {
   // __dirname : 경로 지정하는 시스템변수, 프로젝트 폴더를 루트경로로 찾아준다
   res.sendFile(__dirname + "/index.html"); // 서버측 응답용 URL
@@ -41,10 +41,10 @@ app.get("/about", (req, res) => {
   res.sendFile(__dirname + "/pages/about.html");
 });
 
-// 404
-// app.get("*", (req, res) => {
-//   res.sendFile(__dirname + "/pages/404.html");
-// });
+// 404;
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/pages/404.html");
+});
 
 // 서버 오픈
 app.listen(3000, function () {
